b19c6931c7817a29096db21d32f79edf19a1a9c4
Dalgona <dalgona@hontou.moe>
2017-12-29 10:50:42 +0900

    Create a new project for RPS application

---
+++ apps/rps/lib/rps/application.ex
@@ -0,0 +1,20 @@
+defmodule RPS.Application do
+  # See https://hexdocs.pm/elixir/Application.html
+  # for more information on OTP Applications
+  @moduledoc false
+
+  use Application
+
+  def start(_type, _args) do
+    # List all child processes to be supervised
+    children = [
+      # Starts a worker by calling: RPS.Worker.start_link(arg)
+      # {RPS.Worker, arg},
+    ]
+
+    # See https://hexdocs.pm/elixir/Supervisor.html
+    # for other strategies and supported options
+    opts = [strategy: :one_for_one, name: RPS.Supervisor]
+    Supervisor.start_link(children, opts)
+  end
+end
