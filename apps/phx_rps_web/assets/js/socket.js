3457890c59d3e7fce90f7402f58880da6ae27da0
Dalgona <dalgona@hontou.moe>
2018-01-02 14:01:52 +0900

    Implement RPS socket client

--- apps/phx_rps_web/assets/js/socket.js
+++ apps/phx_rps_web/assets/js/socket.js
@@ -1,13 +1,84 @@
 import {Socket, Presence} from "phoenix"
 
-let socket = new Socket("/socket", {params: {token: window.userToken}})
+function GameSocket(roomInfo) {
+  /** @type {GameSocket} */
+  const that = this;
+  let socket = new Socket("/socket", {params: roomInfo})
+  let presences = {}
+  socket.connect()
 
-socket.connect()
+  let channel = socket.channel(`rps_room:${roomInfo.room_id}`, {})
+  channel.join()
+    .receive("ok", resp => that.connectSucceeded)
+    .receive("error", resp => that.connectFailed)
 
-// Now that you are connected, you can join channels with a topic:
-let channel = socket.channel("topic:subtopic", {})
-channel.join()
-  .receive("ok", resp => { console.log("Joined successfully", resp) })
-  .receive("error", resp => { console.log("Unable to join", resp) })
+  channel.on("rps_room_closed", data => {
+    that.roomClosed(data.room_id)
+  })
 
-export default socket
+  channel.on("rps_game_started", data => {
+    that.gameStarted(data.room_id)
+  })
+
+  channel.on("rps_game_finished", data => {
+    that.gameFinished(data.winners, data.plays, data.room_id)
+  })
+
+  channel.on("rps_play", data => {
+    that.someonePlayed(data.by, data.room_id)
+  })
+
+  channel.on("presence_state", state => {
+    presences = Presence.syncState(presences, state)
+    that.presenceUpdated(presences, Presence)
+  })
+
+  channel.on("presence_diff", diff => {
+    presences = Presence.syncDiff(presences, diff)
+    that.presenceUpdated(presences, Presence)
+  })
+
+  //
+  // Methods
+  //
+
+  /** @type {() => void} */
+  this.close = () => {
+    channel.leave()
+    socket.disconnect()
+  }
+
+  /** @type {() => void} */
+  this.startGame = () => {
+    if (!roomInfo.is_owner) {
+      throw "Cannot start the game: You are not the owner of this room."
+    }
+    channel.push("rps_start_game", {})
+  }
+
+  /** @type {(move: string) => void} */
+  this.play = move => {
+    channel.push("rps_play", {move: move})
+  }
+
+  //
+  // Callbacks
+  //
+
+  /** @type {(response: any) => void} */
+  this.connectSucceeded = resp => { console.log("Successfuly connected:", resp) }
+  /** @type {(response: any) => void} */
+  this.connectFailed = resp => { console.warn("Unable to connect:", resp) }
+  /** @type {(presences: any, presence: Presence) => void} */
+  this.presenceUpdated = null
+  /** @type {(roomId: string) => void} */
+  this.roomClosed = null
+  /** @type {(roomId: string) => void} */
+  this.gameStarted = null
+  /** @type {(winners: [string], plays: any, roomId: string) => void} */
+  this.gameFinished = null
+  /** @type {(by: string, roomId: string) => void} */
+  this.someonePlayed = null
+}
+
+export { GameSocket }
