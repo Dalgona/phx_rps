b9800fc02e07422d7350b05feb2e3c303f176edd
Dalgona <dalgona@hontou.moe>
2018-01-03 11:31:51 +0900

    Write docs for RPS.Room module

--- apps/rps/lib/rps/room.ex
+++ apps/rps/lib/rps/room.ex
@@ -1,9 +1,30 @@
 defmodule RPS.Room do
+  @moduledoc """
+  This module defines a struct which holds state information of a single RPS
+  game room, and operations which can be applied to that struct.
+
+  ## Struct Information
+
+  Currently, the `RPS.Room` struct has the following keys:
+
+  * `:owner` - The name of the player who owns the game room.
+  * `:players` - A list of names of players in the game room.
+  * `:pid_map` - Holds association between player names and processes.
+  * `:plays` - Contains information about move that each player played.
+  * `:status` - The status of the game room. It can hold either `:ready`
+    or `:playing`.
+  """
+
   defstruct [:owner, :players, :pid_map, :plays, :status]
 
   @type t :: %__MODULE__{}
   @type status :: :ready | :playing
+  @type plays :: %{optional(binary) => RPS.move}
   @type result(type) :: {:ok, type} | {:error, term}
+
+  @doc """
+  Creates a new struct representing an RPS game room owned by `owner`.
+  """
 
   @spec new(binary) :: t
 
@@ -17,6 +38,14 @@ defmodule RPS.Room do
     }
   end
 
+  @doc """
+  Adds a player `name` to `room` so that it can be assigned to a process later.
+
+  This function returns a tuple with a newly added player name and the updated
+  room struct. The returned player name may differ from the `name` parameter
+  because this function mangles the name to prevent name collision.
+  """
+
   @spec add_player(t, binary) :: {binary, t}
 
   def add_player(room, name) do
@@ -28,6 +57,12 @@ defmodule RPS.Room do
     }
     {new_name, updated_room}
   end
+
+  @doc """
+  Removes a player `name`, which is not assigned to any process yet.
+
+  Trying to remove an already assigned name will result in error.
+  """
 
   @spec remove_player(t, binary) :: result(t)
 
@@ -44,6 +79,12 @@ defmodule RPS.Room do
     end
   end
 
+  @doc """
+  Assigns a player `name` to the given `process`.
+
+  At most one player name can be assigned to each process.
+  """
+
   @spec assign_player(t, pid, binary) :: result(t)
 
   def assign_player(room, process, name) do
@@ -58,6 +99,10 @@ defmodule RPS.Room do
         {:ok, updated_room}
     end
   end
+
+  @doc """
+  Removes information of a player assigned to the given `process`, from `room`.
+  """
 
   @spec leave(t, pid) :: result(t)
 
@@ -75,13 +120,15 @@ defmodule RPS.Room do
     end
   end
 
-  @spec all_assigned?(t) :: boolean
+  @doc """
+  Starts a new game and changes the status of `room` to `:playing`.
 
-  def all_assigned?(room) do
-    all_players = MapSet.new room.players
-    assigned_players = room.pid_map |> Map.values() |> MapSet.new()
-    0 == all_players |> MapSet.difference(assigned_players) |> MapSet.size()
-  end
+  The following conditions must be met to start the game:
+
+  * The room must be in `:ready` status.
+  * There are two or more players in the room.
+  * All players are assigned to distinct processes.
+  """
 
   @spec start(t) :: result(t)
 
@@ -101,6 +148,10 @@ defmodule RPS.Room do
     end
   end
 
+  @doc """
+  Records a move from a player assigned to the given `process`.
+  """
+
   @spec play(t, pid, RPS.move) :: result(t)
 
   def play(room, process, move) when move in [:rock, :paper, :scissors] do
@@ -118,7 +169,15 @@ defmodule RPS.Room do
     end
   end
 
-  @spec get_result(t) :: {[binary], %{optional(binary) => RPS.move}}
+  @doc """
+  Returns the result of the last game.
+
+  This function returns a tuple with a list of names of players who won
+  the game, and the value of `:plays` key. This function shall not be called
+  until every player plays a move.
+  """
+
+  @spec get_result(t) :: {[binary], plays}
 
   def get_result(%{plays: plays}) do
     grouped_by_moves = Enum.group_by plays, &elem(&1, 1), &elem(&1, 0)
@@ -128,11 +187,21 @@ defmodule RPS.Room do
     end
   end
 
+  @doc """
+  Checks if the player assigned to the given `process` is owner of `room`.
+  """
+
   @spec owner?(t, pid) :: boolean
 
   def owner?(%{owner: owner, pid_map: pid_map}, process) do
     owner == pid_map[process]
   end
+
+  @doc """
+  Returns the name of a player assigned to the given `process`.
+
+  This function will return `nil` if it cannot find any association.
+  """
 
   @spec whois(t, pid) :: binary
 
@@ -141,6 +210,9 @@ defmodule RPS.Room do
   #
   # Private Functions
   #
+
+  # Tries to generate a unique player name.
+  # e.g. PlayerName, PlayerName_0, PlayerName_1, ...
 
   @spec unique_player_name(binary, [binary]) :: binary
 
@@ -163,11 +235,19 @@ defmodule RPS.Room do
     end
   end
 
+  # Checks if all players are assigned to distinct processes.
+
+  @spec all_assigned?(t) :: boolean
+
+  defp all_assigned?(room) do
+    all_players = MapSet.new room.players
+    assigned_players = room.pid_map |> Map.values() |> MapSet.new()
+    0 == all_players |> MapSet.difference(assigned_players) |> MapSet.size()
+  end
+
   @spec winner(RPS.move, RPS.move) :: RPS.move | nil
 
   defp winner(move1, move2)
-
-  defp winner(move, move), do: nil
 
   for {x, y} <- [rock: :scissors, paper: :rock, scissors: :paper] do
     defp winner(unquote(x), unquote(y)), do: unquote(x)
