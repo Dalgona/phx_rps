36487e298695649c3817f0357a105e02e406e2c4
Dalgona <dalgona@hontou.moe>
2018-05-11 16:59:04 +0000

    Bootstrap Elm application

---
+++ apps/phx_rps_web/assets/elm/src/Main.elm
@@ -0,0 +1,9 @@
+module Main exposing (..)
+
+
+import Html exposing (..)
+
+
+main : Html msg
+main =
+  text "Hello, world!"
--- apps/phx_rps_web/assets/js/app.js
+++ apps/phx_rps_web/assets/js/app.js
@@ -1,2 +1,6 @@
 import "phoenix_html"
 import "./ui";
+import Elm from "./elm"
+
+const elmDiv = document.getElementById("elm-main");
+const elmApp = Elm.Main.embed(elmDiv);
